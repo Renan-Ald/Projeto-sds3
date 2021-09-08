16b66e7710d6276ee08b337b69f380db4722aee3
Theown13 <renan.ald@hotmailcom>
2021-09-08 13:46:56 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 import React from 'react';
 function App() {
   return (
-    <div >
+    <>
+  <Navbar/>
+    <div className= "container" >
       <h1 className="text-primary">Olá men </h1>
-    </div>
+      <DataTable/>
+     </div>
+    <Footer/>
+      </>
   );
 }
 
